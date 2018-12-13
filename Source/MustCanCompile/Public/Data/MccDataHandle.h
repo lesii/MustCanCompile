f3b2ece89356b1c2d7126ec356b211f94e2e09bb
lesii <Le_Sii@hotmail.com>
2018-12-13 13:56:07 +0800

    sln Modify

--- Source/MustCanCompile/Public/Data/MccDataHandle.h
+++ Source/MustCanCompile/Public/Data/MccDataHandle.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
-
+#include "Data/MccTypes.h"
 /**
  * 
  */
@@ -11,5 +11,19 @@ class MUSTCANCOMPILE_API MccDataHandle
 {
 public:
 	MccDataHandle();
-	~MccDataHandle();
+
+	static void Initialize();
+	static TSharedPtr<MccDataHandle> Get();
+
+	void ChangeLocalizationCultrue(ECultureTeam _Culture);
+
+public:
+	ECultureTeam CurrentCulture;
+
+private:
+	static TSharedRef<MccDataHandle> Creat();
+
+private:
+	static TSharedPtr<MccDataHandle> DataInstance;
+
 };
